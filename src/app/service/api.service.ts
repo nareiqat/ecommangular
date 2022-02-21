25400bb8756c77439cce5405ddf6af3c929c5b2a
nareiqat <nabeelareiqat@gmail.com>
2022-02-21 12:57:30 -0500

    done with filter, search and addtocart

---
+++ src/app/service/api.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http : HttpClient) { }
+
+
+  getProduct(){
+    return this.http.get<any>("https://fakestoreapi.com/products").pipe((res:any)=>{
+      return res;
+    })
+  }
+}
